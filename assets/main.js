ac4c62c1464ef21d5a8ba40a7db4006025f8445f
Mirine88 <coding.cdh88@gmail.com>
2021-01-16 11:55:05 +0900

    add popup to show ad

--- assets/main.js
+++ assets/main.js
@@ -209,3 +209,21 @@ function initCommand() {
   $('#language-selector').html()
 }
 initCommand()
+
+$(function () {
+  var overlay = $('<div id="overlay"></div>');
+  overlay.show();
+  overlay.appendTo(document.body);
+  $('.popup').show();
+  $('.close').click(function () {
+    $('.popup').hide();
+    overlay.appendTo(document.body).remove();
+    return false;
+  });
+
+  $('.x').click(function () {
+    $('.popup').hide();
+    overlay.appendTo(document.body).remove();
+    return false;
+  });
+});

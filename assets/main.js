e88b2bc1cdd2cb4e1cea319599b4ff23d8d6c9ba
Mirine88 <coding.cdh88@gmail.com>
2021-01-16 12:12:18 +0900

    delete popup

--- assets/main.js
+++ assets/main.js
@@ -209,21 +209,3 @@ function initCommand() {
   $('#language-selector').html()
 }
 initCommand()
-
-$(function () {
-  var overlay = $('<div id="overlay"></div>');
-  overlay.show();
-  overlay.appendTo(document.body);
-  $('.popup').show();
-  $('.close').click(function () {
-    $('.popup').hide();
-    overlay.appendTo(document.body).remove();
-    return false;
-  });
-
-  $('.x').click(function () {
-    $('.popup').hide();
-    overlay.appendTo(document.body).remove();
-    return false;
-  });
-});
